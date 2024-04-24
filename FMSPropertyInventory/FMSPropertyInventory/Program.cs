3ee318433cb1f75133a9ace8f6f84d619d305984
Mark <108047458+AEArtemis@users.noreply.github.com>
2024-04-24 08:46:22 +0800

    .

--- FMSPropertyInventory/FMSPropertyInventory/Program.cs
+++ FMSPropertyInventory/FMSPropertyInventory/Program.cs
@@ -15,7 +15,7 @@ namespace FMSPropertyInventory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmLandInventory());
         }
     }
 }
